70aabc37e47a7961a3d31c13fb79946c40276108
bowline90 <andrea.ferraris90@gmail.com>
2019-09-09 16:08:21 +0200

    Fixing path

--- binrida/FridaHandler.py
+++ binrida/FridaHandler.py
@@ -57,9 +57,9 @@ class FridaHandler(bn.BackgroundTaskThread):
         time.sleep(1)
         bn.log.log_info('Mapping:'+hex(self.base)+' - '+hex(self.end));
         
-        path = bn.user_plugin_path()+'/binrida/binrida/'
+        path = bn.user_plugin_path()+'/BinRida/binrida/'
         if not os.path.isfile(path+'stalker.js'):
-            path = bn.bundle_plugin_path()+'/binrida/binrida/'
+            path = bn.bundle_plugin_path()+'/BinRida/binrida/'
             if not os.pth.isfile(path+'stalker.js'):
                 bn.log.log_error('Javascript code not found!')
                 return
